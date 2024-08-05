f76bcd6a9898756da63b701f82c0a103dd9beb50
Jaipal Singh Sisodiya <jaipalsinghsisodiya46@gmail.com>
2024-08-05 22:00:59 +0530

    App javascript update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,39 @@
 /**
- * @param{NodeList}$ elements 
- * @param{*}eventType
- * @param{*}callback
+ * Add eventlisterner on multiple elements
+ * @param{NodeList}$ elements  NodeList
+ * @param{Sttring}eventType string
+ * @param{Function}callback Function
  */
 
 
 'use strict';
 
-const 
+const addEventOnElement = function($elements, eventType, callback){
+ for(const $item of $elements){
+    $item.addEventListener(eventType,callback);
+ }
+}
+
+/**
+ * Header scroll state
+ */
+
+const /**{NodeElement} */ $header = document.querySelector("[data-header]");
+window.addEventListener("scroll",function (){
+   $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
+
+/**
+ * Search toggle
+ */
+
+const/**{NodeElement} */ $searchToggler = document.querySelector("[data-search-toggler]");
+const/**{NodeElement} */$searchField = document.querySelector("[data-search-field]");
+let /**{Boolean} */ isExpended = false;
+
+$searchToggler.addEventListener("click", function(){
+   $header.classList.toggle("search-active");
+   isExpended = isExpended ? false : true;
+   this.setAttribute("aria-expended", isExpended);
+   $searchField.focus();
+})
